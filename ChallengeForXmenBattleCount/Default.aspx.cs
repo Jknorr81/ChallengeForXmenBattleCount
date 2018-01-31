83ca5898f56d38fadcda26cb86f7bfb4c74220b1
James Knorr <jknorr@coachmeapps.com>
2018-01-30 16:10:10 -0800

    upload challenge

---
+++ ChallengeForXmenBattleCount/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeForXmenBattleCount
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "John", "suzy", "Ethan" };
+            int[] battles = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+
+            string result = "";
+
+            int highestNumberOfBattles = battles.Max();
+            int lowestNumberOfBattles = battles.Min();
+
+            for (int i = 0; i < battles.Length; i++)
+            {
+                if (battles[i] == highestNumberOfBattles)
+                {
+                    result += string.Format("Most battles belong to: {0} (Value: {1})<br />", names[i], battles[i]);
+                }
+                if (battles[i] == lowestNumberOfBattles)
+                {
+                    result += string.Format("Least battles belong to: {0} (Value: {1})<br />", names[i], battles[i]); 
+                }
+            }
+
+            resultLabel.Text = result;
+
+
+
+        }
+    }
+}
